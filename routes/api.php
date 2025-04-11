8c24fcbb834905a228c65390bc774901f11731e6
SayaDimas <dimasrifkinuramadani@gmail.com>
2025-04-11 10:17:41 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,24 @@
 <?php
-
 use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Routes untuk autentikasi (login, register, dll)
+// Route login & register (tambahkan name pada login jika ingin hindari error route not found)
 Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
+    Route::post('login', 'login')->name('login'); //
     Route::post('register', 'register');
-    // Route::post('logout', 'logout');
-    // Route::post('refresh', 'refresh');
 });
 
+// Route yang butuh autentikasi JWT
+Route::middleware('auth:api')->group(function () {
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+// Resource route
 Route::apiResource('/students', StudentController::class);
